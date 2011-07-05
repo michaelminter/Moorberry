63d0eb9117a2120390872fa8132b359de63cc4d6
michaelminter <mothore@gmail.com>
2011-07-05 17:58:03 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,276 @@
+<!--
+                ,)(8)).
+              (()))())()).
+             (()"````"::= )
+             )| _    _ ::= )
+            (()(o)/ (o) ?(/)
+             )(::c ::.( :(/)
+            (( \ .__. ;,/(/)
+              ) `.___,'/ (/)
+                 |    | (/)
+               _.'    ,\(/)__
+           _.-"   ` u    (/) ".
+         ,"               ^    \           HANDS OFF
+        /                      |           -- my code (c) 2011 Moorberry Solutions
+        |           .      `.  |
+        |   /,'   _  `.'   _ `.|
+        |   ||   (o)  |   (o) ||
+        |   |\        ;       /)
+        (   \ `.,___,' `.,__,'/
+         \   \ |           | /
+          \   `;           |/
+           `. /            |
+             Y             |
+            /          (   |
+           /               ;
+          /               /
+         ;       ` .    ,'(
+         |          \##'   \
+         :           Y      \
+          \           \      \
+           \           \      \
+            \           \      \
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Moorberry Solution | Michael Minter</title> <!-- The websites title. You can change this to whatever you want. -->
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="Content-Script-Type" content="text/javascript" />
+	<meta http-equiv="Content-Language" content="en" /> 
+	<meta http-equiv="Cache-control" content="no-cache" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="robots" content="index,follow" /> 
+	<meta name="revisit-after" content="2 weeks" /> <!-- Tells the search engines (Google, MSN, Yahoo...etc) bots when to revisit your website.  -->
+
+	<meta name="google-site-verification" content="non4BBenU1Es_7fI6bhHoZ3wmir683PPc-ZRdJBQJyk" />
+
+	<link type="text/css" rel="stylesheet" media="screen" href="coffee.css" /> <!-- The websites CSS file. Keep in mind that you have to change this line if you rename or move the CSS file. -->
+	<link type="text/css" rel="stylesheet" media="screen" href="slimbox/css/slimbox.css" /> <!-- The Lightboxs CSS file. Keep in mind that you have to change this line if you rename or move the CSS file.  -->
+	<link rel="shortcut icon" href="http://gravatar.com/avatar/8afb20e206dc3d2e010ebbe812ebaa09.png?s=16" title="Favicon" />
+  
+	<script type="text/javascript" src="smoothscroll.js"></script> <!-- The Smoot Scrool script. Keep in mind that you have to change this line if you rename or move the CSS file. -->
+	<script type="text/javascript" src="slider/js/jquery.js"></script> <!-- The Slider script. Keep in mind that you have to change this line if you rename or move the CSS file. -->
+	<script type="text/javascript" src="slider/js/easySlider1.5.js"></script> <!-- The Slider script. Keep in mind that you have to change this line if you rename or move the CSS file. -->
+	<script src="slimbox/js/slimbox.js" type="text/javascript"></script> <!-- The Lightbox script. Keep in mind that you have to change this line if you rename or move the CSS file. -->
+	<script type="text/javascript" src="contact.js"></script>
+	<!--[if IE 7]>
+	<script defer type="text/javascript" src="pngfix.js"></script>
+	<![endif]-->
+  
+<script type="text/javascript">
+	$(document).ready(function(){	
+		$("#slider").easySlider({
+			auto: true, <!-- The Slider script. "true" keeps it running automatic, "false" turns the automatic movement off.  -->
+			continuous: true <!-- The Slider script. "true" keeps it running continuing, "false" turns it off.  -->
+		});
+		
+		$("#menu li a").click(function() {
+			$("#menu li a.current").removeClass("current");
+			$(this).addClass("current");			
+		});
+				
+		$("#sendmessage").click(function() {
+			$("#menu li a.current").removeClass("current");
+			$("#contact-link").addClass("current");			
+		});
+	});	
+</script>
+
+</head>
+<body>
+<div id="wrapper">
+	<div id="anchor"><a name="top" id="top"></a></div>
+	<div id="header">
+		<div id="header-m1"></div>
+		<h1 id="logo"><a href="/">Michael Minter</a></h1>
+		<div id="header-m2"></div>
+		<div id="navigation"> 		
+			<a href="#top">HOME</a>
+			<a href="#about">ABOUT</a>
+			<a href="#folio">PORTFOLIO</a>
+			<a href="#contact">CONTACT</a>
+			<a href="http://blog.moorberry.biz">BLOG</a>
+		</div>
+	</div>
+		<div id="slide-wrapper">
+			<div id="slider">
+				<ul>
+					<li><a href="#folio"><img src="slider/images/captive.jpg" /></a></li>
+					<li><a href="#folio"><img src="slider/images/tuna.jpg" alt="Css Template Preview" /></a></li>
+					<li><a href="#folio"><img src="slider/images/opentag_sm.png" alt="Css Template Preview" /></a></li>
+					<li><a href="#folio"><img src="slider/images/gold.jpg" alt="Css Template Preview" /></a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="header-content" style="display:none;">
+			<h1>WORKING WITH WEB DESIGNERS & SMALL BUSINESSES TO GET THINGS DONE</h1>
+		</div>
+		<div id="homepage-wrapper">
+			<div id="homepage-m1"></div>
+			<div id="homepage-c1">
+    		<h2>SOFTWARE</h2>
+			<img src="graphic/process.png" style="float:left;margin-right:10px;">
+			<p>Frequent services I perform include; but are not limited to; data integration and management control, Paypal checkout, and user authentication.</p>
+			<p><a href="http://github.com/michaelminter" target="_blank">fork me <img src="graphic/github2_16.png" style="margin-bottom:-5px;" /> at github</a></p>
+		</div>
+		<div id="homepage-m2"></div>
+		<div id="homepage-c2">
+			<h2>HOSTING</h2>
+			<p><img src="graphic/server.png" style="float:left;margin-right:10px;">Pick Moorberry Solutions as your hosting-provider, and <strong>never</strong> worry about compliancy.</p>
+			<p>Hosting currently <strong>unavailable</strong> due to overwhelming responses. <a href="http://blog.godhacked.com">Stay tuned</a> for coming availability.</p>
+		</div>
+		<div id="homepage-m3"></div>
+		<div id="homepage-c3">
+			<h2>REFERRAL</h2>
+			<p><img src="graphic/taxes.png" style="float:left;margin-right:10px;">Earn up to <strong>15%</strong> of the final project fee for every person you refer to Moorberry Solutions.</p>
+			<p>Affiliate accounts will be accepted by application. Please <a href="#contact">contact me</a>, with traffic analysis, for more information.</p>
+		</div>
+	</div>
+	<br class="dirtyLittleTrick"  />
+	<!-- Separator || Do Not Remove or Change -->
+	<div class="divider">
+		<img src="graphic/separator_line_left.gif" alt="" width="412" height="28" /><a href="#top"><img src="graphic/separator_line_button.gif" alt="" width="28" height="28" /></a><img src="graphic/separator_line_right.gif" alt="" width="410" height="28" />    </div>
+	<!-- End Speparator || Do not Remove -->
+	<div id="about-wrapper">
+		<div id="about-m1"></div>
+		<div id="about-c1">
+			<h2>HELLO, MY NAME IS MICHAEL MINTER</h2>
+			<div style="background:transparent url(graphic/picture.png) no-repeat 0 0;float:left;height:179px;padding:15px 0 0 15px;width:179px;"><img src="http://gravatar.com/avatar/8afb20e206dc3d2e010ebbe812ebaa09.png?d=mm&r=PG&s=149" /></div>
+			<p>I currently reside in Austin, TX and I have a passion for developing great usable interfaces.</p>
+			<p>Although...</p>
+		</div>
+		<div id="about-m2"><a name="about" id="about"></a></div>
+		<div id="about-c2">
+			<h2>SKILLS ASSESSMENT</h2>
+			<p><strong>I create beautiful works of art.</strong><br />Though my art is mostly in the code.</p>
+			<table width="250" cellpadding="5" cellspacing="0" style="margin-bottom:20px;">
+				<tr>
+					<td width="170">Zend PHP & Framework</td>
+					<td width="80" style="text-align:right;">
+						<img src="graphic/check.png" style="margin-right:3px;"><img src="graphic/check.png" style="margin-right:3px;"><img src="graphic/check.png">
+					</td>
+				</tr>
+				<tr style="background:#EFEFEF;">
+					<td width="170">UI Design</td>
+					<td width="80" style="text-align:right;">
+						<img src="graphic/check.png" style="margin-right:3px;"><img src="graphic/check.png" style="margin-right:3px;"><img src="graphic/check.png">
+					</td>
+				</tr>
+				<tr>
+					<td width="170">Ruby on Rails</td>
+					<td width="80" style="text-align:right;">
+						<img src="graphic/check.png" style="margin-right:3px;"><img src="graphic/check.png">
+					</td>
+				</tr>
+				<tr style="background:#EFEFEF;">
+					<td width="170">Javascript (JQuery)</td>
+					<td width="80" style="text-align:right;">
+						<img src="graphic/check.png" style="margin-right:3px;" /><img src="graphic/check.png" />
+					</td>
+				</tr>
+			</table>   
+		</div>
+	</div>
+	<br class="dirtyLittleTrick"  />
+	<!-- Separator || Do Not Remove or Change -->
+	<div class="divider">
+		<img src="graphic/separator_line_left.gif" alt="" width="412" height="28" /><a href="#top"><img src="graphic/separator_line_button.gif" alt="Go top" width="28" height="28" /></a><img src="graphic/separator_line_right.gif" alt="" width="410" height="28" />  </div>
+	<!-- End Speparator || Do not Remove -->
+	<div id="folio-wrapper">
+		<a name="folio" id="folio"></a>
+		<h2>SOME OF MY FAVORITE PROJECTS</h2>
+		<table width="850" border="0" cellpadding="0" cellspacing="0" class="table-margin">
+			<tr>
+				<td width="268" height="178" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/captive.jpg" alt="IMG" width="268" height="158" /></a></td>
+				<td width="22" height="178" align="left" valign="top">&nbsp;</td>
+				<td width="268" height="178" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/beaufort2_thm.png" alt="IMG" width="268" height="158" /></a></td>
+				<td width="22" height="178" align="left" valign="top">&nbsp;</td>
+				<td width="268" height="178" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/tehouse_thm.png" alt="IMG" width="268" height="158" /></a></td>
+			</tr>
+			<tr>
+				<td width="268" height="158" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/opentag.png" alt="IMG" width="268" height="158" /></a></td>
+				<td width="22" height="158" align="left" valign="top">&nbsp;</td>
+				<td width="268" height="158" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/search.png" alt="IMG" width="268" height="158" /></a></td>
+				<td width="22" height="158" align="left" valign="top">&nbsp;</td>
+				<td width="268" height="158" align="left" valign="top"><a href="folio/preview_image.png" rel="lightbox" title="Put Description Here" ><img src="folio/gold.png" alt="IMG" width="268" height="158" /></a></td>
+			</tr>
+		</table>      
+	</div>
+	<br class="dirtyLittleTrick"  />
+	<!-- Separator || Do Not Remove or Change -->
+	<div class="divider">
+		<img src="graphic/separator_line_left.gif" alt="" width="412" height="28" /><a href="#top"><img src="graphic/separator_line_button.gif" alt="Go top" width="28" height="28" /></a><img src="graphic/separator_line_right.gif" alt="" width="410" height="28" />  </div>
+	<!-- End Speparator || Do not Remove -->
+	<div id="contact-wrapper">
+		<div id="contact-m1"></div>
+		<div id="contact-c1">
+			<h2>QUESTIONS? GET IN TOUCH WITH ME</h2>
+			<p><strong>Thank you for visiting!</strong> If you are inquiring about a redesign, please submit your company website's URL, along with details of services needed. I will reply back as soon as possible.</p>
+			<br />
+			<div id="note"></div>
+			<div id="fields">
+				<form name="contact_form" method="post" action="contact.php">
+					<table width="550" border="0" cellpadding="0" cellspacing="0">
+						<tr>
+							<td width="225" valign="top">
+								<strong>Name:</strong> 
+								<input name="name" type="text" class="form_nm" id="Name:" />
+								<br /><br />
+								<strong>Email:</strong> 
+								<input name="email" type="text" class="form_nm" id="Email:" />
+								<br />
+							</td>
+							<td width="325" valign="top">
+								<div align="right">
+									<textarea name="message" class="form_tf" cols="" rows="" id="Message" placeholder="Message:"></textarea>
+									<br /><br />
+									<input name="send_button" src="graphic/send.gif" alt="" title="Send" type="image" />
+								</div>
+							</td>
+						</tr>
+					</table>
+				</form>
+			</div>
+		</div>
+		<div id="contact-m2"><a name="contact" id="contact"></a></div>
+		<div id="contact-c2">
+			<h2>CONTACT DETAILS</h2>
+			<p><strong>Moorberry Solutions</strong><br />
+				Michael Minter<br />
+				<i>Austin, Texas, United States</i><br /><br />
+			</p>
+			<p>
+				<a href="http://www.linkedin.com/in/mminter"><img src="icon/linkedin-32x32.png" style="float:left;margin-right:5px;"><span style="display:block;padding-top:5px;">Linkedin</span></a><br />
+				<a href="http://github.com/michaelminter" target="_blank"><img src="graphic/github.png" style="float:left;margin-right:5px;"><span style="display:block;padding-top:5px;">Github</span></a>	
+			</p>
+			<p>
+				<a href="http://www.twitter.com/mothore" target="_blank"><img src="graphic/twitter.gif" alt="Twitter" width="250" height="72" border="0" /></a>
+			</p>
+		</div>
+	</div>
+	<br class="dirtyLittleTrick"  />
+	<!-- Separator || Do Not Remove or Change -->
+	<div class="divider">
+		<img src="graphic/separator_line_left.gif" alt="" width="412" height="28" /><a href="#top"><img src="graphic/separator_line_button.gif" alt="Go top" width="28" height="28" /></a><img src="graphic/separator_line_right.gif" alt="" width="410" height="28" />  </div>
+	<!-- End Speparator || Do not Remove -->
+	<div id="copy">
+		<div align="center">Copryight © 2011 Moorberry Solutions - All rights reserved
+			-  <a href="http://validator.w3.org/check/referer" target="_blank"> 
+			<span class="Stil2">XHTML</span></a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+			<span class="Stil2">CSS</span></a>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6309656-5");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
